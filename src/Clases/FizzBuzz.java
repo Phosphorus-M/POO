c5e86c3b2b728ef3673a8b275d9375cf802155c8
Phosphorus Moscu <fernando.n.p.d.moscu@gmail.com>
2017-09-01 01:27:23 -0300

    Actividades del trabajo 3

---
+++ src/Clases/FizzBuzz.java
@@ -0,0 +1,27 @@
+/**
+ * Imprimir por pantalla los números del 1 al 100 pero considerando lo siguiente:
+ * Si el número es divisible por 3 se debe imprimir “Fizz”.
+ * Si el número es divisible por 5 se debe imprimir “Buzz”.
+ * Si el número es divisible por 3 y por 5 se debe imprimir “FizzBuzz”.
+ * @author: Phosphorus Moscu / Fernando Pastorelli
+ * @version: v1.0 1/09/2017
+ * @see <a href = "https://github.com/Phosphorus-M/Java" /> Clases de Java </a>
+ */
+package Clases;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		for(int i=0;i<=100;i++){
+			System.out.println("El numero i es: " + i);
+			if((i%3)==0) {
+				System.out.println("Fizz");
+			}
+			if((i%5)==0) {
+				System.out.println("Buzz");
+			}
+			if((i%5)==0 && (i%3 == 0 )) {
+				System.out.println("FizzBuzz");
+			}
+		}
+	}
+}
