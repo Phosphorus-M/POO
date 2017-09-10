bd07ca5d2a0130629e9b16f765aa7520c91401ba
Phosphorus Moscu <fernando.n.p.d.moscu@gmail.com>
2017-09-10 18:52:20 -0300

    Aula

--- src/Clases/Examen.java
+++ src/Clases/Examen.java
@@ -1,6 +1,6 @@
 /**
  * @author: Phosphorus Moscu / Fernando Pastorelli
- * @version: v1.2 10/09/2017
+ * @version: v1.3 10/09/2017
  * @see <a href = "https://github.com/Phosphorus-M/Java"> Clases de Java </a>
  */
 package Clases;
@@ -12,10 +12,13 @@ public class Examen {
 	private String materia = null;
 	private fecha Fecha = null;
 	private Double nota = null;
-	public Examen(String materia, Double nota, Scanner fechas){
+	private Integer aula =null;
+	
+	public Examen(String materia, Double nota, Scanner fechas, Integer aula){
 		this.IngresarFecha(fechas);
 		this.materia = materia;
 		this.nota = nota;
+		this.aula = aula;
 	}
 	public fecha IngresarFecha(Scanner fechas) {
 		this.Fecha = new fecha();
@@ -31,6 +34,7 @@ public class Examen {
 		}//Si fecha es null no saldra del bucle y preguntara de nuevo
 		return this.Fecha; 
 	}
+	
 	public static void main(String[] args) {
 		ArrayList<Examen> examen = new ArrayList<Examen>();
 		Scanner nombre1 = new Scanner(System.in);//Escanea la nombre de la consola
@@ -39,7 +43,7 @@ public class Examen {
 		
 		String nombre = "a";//Inicializa nombre en algo
 		Double nota;
-		
+		Integer aula;
 		while(!nombre.equals("Salir")){
 			System.out.println("Ingresar el nombre de la materia");
 			nombre = nombre1.nextLine(); //Obtiene el nombre
@@ -51,15 +55,23 @@ public class Examen {
 						System.out.println("Debe ingresar un valor entre 0 y 10");
 					}
 				}while(nota<0 || nota>10);//Se comprueba si la nota es valida
-				Examen examen1 = new Examen(nombre, nota, fechas);//Se crea un objeto con los valores nombre y nota, pero además se envia el scanner de fecha
+				do {
+					System.out.println("Ingresar la aula del examen");
+					aula = notas.nextInt();//Se lee la nota
+					if(aula<0){
+						System.out.println("Debe ingresar un valor positivo");
+					}
+				}while(aula<0);
+				Examen examen1 = new Examen(nombre, nota, fechas, aula);//Se crea un objeto con los valores nombre y nota, pero además se envia el scanner de fecha
 				examen.add(examen1);//Agregara el examen a la lista
 			}
 		}
 		fechas.close();//Se deja de escanear la consola
 		nombre1.close();//Se deja de escanear la consola
 		notas.close();//Se deja de escanear la consola
+		
 		for(Integer i=0; i < examen.size(); i++) {//Imprimira la cantidad de examenes realizados
-			System.out.println(examen.get(i).materia + " " + examen.get(i).Fecha.MostrarDia() + "/" + examen.get(i).Fecha.MostrarMes() + "/" +examen.get(i).Fecha.MostrarAño() + " " + examen.get(i).nota+ " " + examen.get(i).Fecha.Mostrafecha());
+			System.out.println("Materia: " + examen.get(i).materia + " Fecha: " + examen.get(i).Fecha.MostrarDia() + "/" + examen.get(i).Fecha.MostrarMes() + "/" +examen.get(i).Fecha.MostrarAño() + " Nota:" + examen.get(i).nota+ " " + examen.get(i).Fecha.Mostrafecha() + " Aula:" + examen.get(i).aula);
 		}
 
 	}
